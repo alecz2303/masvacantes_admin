43520c37b6de9f57192defcedb6adfc6f0dafc5b
Alejandro Rueda <alecz23@live.com>
2024-02-11 11:43:23 -0600

    MV-14 first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    $posts = Post::all();
+    Debugbar::info($posts);
+    Log::alert("message");
+    return view('home', compact('posts'));
+});
+
+Route::get('post/{slug}', function($slug){
+	$post = Post::where('slug', '=', $slug)->firstOrFail();
+	return view('post', compact('post'));
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
