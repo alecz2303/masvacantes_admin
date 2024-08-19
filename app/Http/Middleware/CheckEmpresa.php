0ac995ab613441f4f0600f819b518009007d8e5c
Alejandro Rueda <arueda@boldrimpact.com>
2024-08-19 10:14:25 -0600

    MVA-60 obligar a crear empresa

---
+++ app/Http/Middleware/CheckEmpresa.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\Empresa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckEmpresa
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        if($request->is('admin/login')){
+            return $next($request);
+        }
+
+        // Obtén el usuario autenticado
+        $user = Auth::user();
+
+        if ($user->role->name === 'Empresa') {
+            // Excluye la ruta de creación de empresa de la verificación
+            if ($request->is('admin/empresas/create') || $request->is('admin/logout') || $request->is('admin/empresas*')) {
+                return $next($request);
+            }
+
+            // Verifica si hay registros en la tabla de Empresas
+            if (!Empresa::where('user_id', $user->id)->exists()) {
+                // Si no hay registros, redirige a una vista específica
+                return redirect()->route('voyager.empresas.create')->with([
+                    'message'    => __('Es necesario crear una Empresa.'),
+                    'alert-type' => 'warning',
+                ]); // Cambia 'empresa.create' al nombre de la ruta de la vista que deseas mostrar
+            }
+
+        }
+
+        return $next($request);
+    }
+}
